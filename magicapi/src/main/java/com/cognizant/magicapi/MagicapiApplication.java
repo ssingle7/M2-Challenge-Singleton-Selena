aca9b5243ea9d3eb731e684a737363d9f5d4dfc8
Selena Singleton <ssingle7@gmail.com>
2021-06-07 19:09:19 -0400

    set up file structure

---
+++ magicapi/src/main/java/com/cognizant/magicapi/MagicapiApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.magicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicapiApplication.class, args);
+	}
+
+}
---
+++ magicapi/src/test/java/com/cognizant/magicapi/MagicapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.magicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
